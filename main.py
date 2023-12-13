03ff11f2e29281ed5e176871182b3f7b9297d668
Soulbolt <soulbolt@gmail.com>
2023-12-13 11:08:12 -0700

    Created a function to perform a "random walk" changing color with each step taken

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ don = Turtle()
 # Change shape of cursor and it's color
 don.shape("turtle")
 don.color("purple4")
-colors = ["blue", "red", "yellow", "purple", "orange", "green", "pink", "DarkOrchid"]
-
 
 # # Make the turtle move to create a square
 # for _ in range(4):
@@ -30,18 +28,31 @@ colors = ["blue", "red", "yellow", "purple", "orange", "green", "pink", "DarkOrc
 #     don.forward(20)
 #     don.right(pentagon_angle)
 # Create a function to draw shapes
-def draw_shape(number_sides):
-    angle = 360 / number_sides
-    for _ in range(number_sides):
-        don.forward(100)
-        don.right(angle)
+# def draw_shape(number_sides):
+#     angle = 360 / number_sides
+#     for _ in range(number_sides):
+#         don.forward(100)
+#         don.right(angle)
+#
+# # Add a different color for each shape being drawn
+colors = ["blue", "red", "SlateGray", "purple", "orange", "green", "pink", "DarkOrchid"]
+
+# for side_num in range(3, 11):
+#     don.color(random.choice(colors))
+#     draw_shape(side_num)
+# Create a random walk function which changes colors for each step taken, and make pen size bigger
+directions = [0, 90, 180, 270]
+don.pensize(15)
 
 
-for side_num in range(3, 11):
-    don.color(random.choice(colors))
-    draw_shape(side_num)
+def random_walk(steps):
+    for turn in range(steps):
+        don.color(random.choice(colors))
+        don.forward(30)
+        don.setheading(random.choice(directions))
 
 
+random_walk(200)
 # open drawing screen until its clicked
 screen = Screen()
 screen.exitonclick()
