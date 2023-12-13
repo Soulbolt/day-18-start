06a62db6cc356adf4a126c5d7817ebb09ea4699b
Soulbolt <soulbolt@gmail.com>
2023-12-13 09:29:55 -0700

    Added code to draw a dashed line

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from turtle import Turtle, Screen
 
 # initialize object
-timmy_the_turtle = Turtle()
+don = Turtle()
 # Change shape of cursor and it's color
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("purple4")
-# Make the turtle move to create a square
-for _ in range(4):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.right(90)
+don.shape("turtle")
+don.color("purple4")
+# # Make the turtle move to create a square
+# for _ in range(4):
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.right(90)
+# Create a dashed line
+for _ in range(20):
+    don.forward(10)
+    don.penup()
+    don.forward(10)
+    don.pendown()
+
+
 
 # open drawing screen until its clicked
 screen = Screen()
